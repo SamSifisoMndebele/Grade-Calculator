d202252eadcc6923f2e3740678116c6fe6ba58ae
Sam Sifiso Mndebele <sams.mndebele@gmail.com>
2025-05-24 18:31:27 +0200

    Setup Compose Desktop project with Gradle configuration
    Initializes a Compose Desktop project with Kotlin and Gradle. Added necessary files for project structure, Gradle setup, and an example main application using Compose UI framework. Includes IntelliJ IDEA-specific configuration and `.gitignore`.

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,35 @@
+import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+
+@Composable
+@Preview
+fun App() {
+    var text by remember { mutableStateOf("Hello, World!") }
+
+    MaterialTheme {
+        Column {
+            Button(onClick = {
+                text = "Hello, Desktop!"
+            }) {
+                Text(text)
+            }
+            MyGrader()
+        }
+    }
+}
+
+fun main() = application {
+    Window(onCloseRequest = ::exitApplication) {
+        App()
+    }
+}
